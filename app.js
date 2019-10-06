f830784c4d91f6ca8f48c4e8518ba4163eb4e551
Vinicius Cardoso <viniciuscc10@gmail.com>
2019-10-05 22:11:15 -0300

    v0.1.0 - Atualizei o arquivo agora usando o better-sqlite3 mais rápido e deletei tudo que não era necessário.

--- app.js
+++ app.js
@@ -1,60 +1,25 @@
+
 var can = require('socketcan');
 var buffer = require('buffer');
 
 // SQLite3 - Database
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./data.db');
-var check;
+const Database = require('better-sqlite3');
+const db = new Database('database.db');
 
 // Work Better with timestamp
 var moment = require('moment');
 
-// Work with Files
-// const fs = require('fs');
 
+// Create Table If Not Exist
+db.exec("CREATE TABLE if not exists data (ts INTEGER,ts_u INTEGER,ts_complete REAL,data_time TEXT,module TEXT,info TEXT,upload INTEGER)");
 
-// Sequencia
-// create_file(); // Criar arqiovo
-
-
-// let userDB = new sqlite3.Database("./user1.db", 
-//     sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, 
-//     (err) => { 
-//         // do your thing 
-//     });
-
-
-// db.close((err) => {
-//   if (err) {
-//     console.error(err.message);
-//   }
-//   console.log('Close the database connection.');
-// });
-
-db.serialize(function() {
-
-  db.run("CREATE TABLE if not exists data (ts INTEGER,ts_u INTEGER,ts_complete REAL,data_time TEXT,module TEXT,info TEXT,upload INTEGER)");
-
-});
-
-// db.close();
-
-
-
-
-
+// Database Prapere to Add Row
+const stmt = db.prepare("INSERT INTO data(ts,ts_u,ts_complete,data_time,module,info,upload) VALUES(?, ?, ?, ?, ?, ?, ?)");
 
 
 var channel = can.createRawChannel("can0", true /* ask for timestamps */);
-channel.start();
-
-function toHex(number) {
-  return (number.toString(16)).slice(-8);
-}
 
 function dumpPacket(msg) {
-
-	// fs.appendFile(file_name,'\n '+log_messege);
 
 	ts = msg.ts_sec;
 	ts_u = msg.ts_usec;
@@ -64,30 +29,16 @@ function dumpPacket(msg) {
 	mod = toHex(msg.id);
 	data = msg.data.toString('hex');
 	
-		db.run('INSERT INTO data(ts,ts_u,ts_complete,data_time,module,info,upload) VALUES(?, ?, ?, ?, ?, ?, ?)', [ts,ts_u,ts_complete,data_time,mod,data,0], (err) => {
+	const add = stmt.run(ts,ts_u,ts_complete,data_time,mod,data,0);
+	console.log("Row Add "+ts+" "+ts_u+" "+ts_complete+" "+data_time+" "+mod+" "+data+" "+ts_complete_2);
 
-			if(err){
-				return console.log(err.message);
-			}
-				console.log("Row Add "+ts+" "+ts_u+" "+ts_complete+" "+data_time+" "+mod+" "+data+" "+ts_complete_2);
-			});
-
-			// db.close();
 }
 
+// To Hex
+function toHex(number) {
+  return (number.toString(16)).slice(-8);
+}
+
+// Executes
+channel.start();
 channel.addListener("onMessage", dumpPacket);
-
-
-
-// Função de Criar o arquivo
-	function create_file(){
-
-		data_atual = moment().format('DD-MM-YYYY__HH-mm-ss-SSS');
-		file_name = data_atual+'.txt';
-
-		fs.appendFile(file_name,'',function (err) {
-		  if (err) throw err;
-		  console.log(file_name+' Created!');
-		});
-
-	}
